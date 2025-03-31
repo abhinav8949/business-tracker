8428e5956ae9d6795c8c514f00cdfaccf1d86938
abhinavamar <abhinavday@gmail.com>
2025-03-31 21:20:49 +0530

    BT: adding router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NetworkService } from './core/services/network.service';
-import { FirebaseMessagingService } from './core/services/firebase-messaging.service';
 
 @Component({
   selector: 'app-root',
@@ -10,41 +8,6 @@ import { FirebaseMessagingService } from './core/services/firebase-messaging.ser
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  title = 'expense-tracker';
-  
-  isOffline$;
-  showOnlineBanner$;
-
-  constructor(private readonly networkService:NetworkService,
-              private fcmService:FirebaseMessagingService
-  ){
-    this.isOffline$ = this.networkService.isOffline$;  // ✅ Assign inside constructor
-    this.showOnlineBanner$ = this.networkService.shownOnlineBanner$;  // ✅ Assign inside constructor
-  }
-
-  ngOnInit(): void {
-    this.fcmService.requestPermission();
-    this.fcmService.listenForMessages()
-  }
-
-  deferredPrompt: any;
-  showInstallButton = false;
-
-  @HostListener('window:beforeinstallprompt', ['$event'])
-  onBeforeInstallPrompt(event: any) {
-    event.preventDefault();
-    this.deferredPrompt = event;
-    this.showInstallButton = true;
-  }
-
-  installPWA() {
-    if (this.deferredPrompt) {
-      this.deferredPrompt.prompt();
-      this.deferredPrompt.userChoice.then(() => {
-        this.deferredPrompt = null;
-        this.showInstallButton = false;
-      });
-    }
-  }
+export class AppComponent{
+  title = 'business-tracker';
 }
